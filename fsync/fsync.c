758b60be86d2ba2c28f7f10e9b5a4a806f519def
icebarf <sysgrammer@protonmail.com>
2022-05-24 12:06:59 +0530

    document the code

--- fsync/fsync.c
+++ fsync/fsync.c
@@ -15,11 +15,18 @@ void print_help(void)
 
 int main(int argc, char* argv[])
 {
+    /* Initial argument checking */
     if (argc < 2 || argc > 2) {
         print_help();
         return 1;
     }
+    if (strncmp(argv[1], "--help", strnlen(argv[1], strlen("--help"))) == 0)
+    {
+        print_help();
+        return 0;
+    }
 
+    /* check if argument file exists and we required permissions */
     if (access(argv[1], F_OK) != 0) {
         printf("fsync: error: %s: No file or directory\n", argv[1]);
         return 1;
@@ -29,18 +36,21 @@ int main(int argc, char* argv[])
         return 1;
     }
 
+    /* open a file stream to argument */
     FILE* fp = fopen(argv[1], "a+");
     if (fp == NULL) {
         printf("fsync: error: %s: Unable to open file - %s\n", argv[1], strerror(errno));
         return 1;
     }
 
+    /* get a file descriptor to argument file stream*/
     int fd = fileno(fp);
     if (fd == -1) {
         printf("fsync: error: %s: file stream is not associated with an actual file\n", argv[1]);
         return 1;
     }
 
+    /* finally perform the syscall */
     int rt = fsync(fd);
     if (rt == -1) {
         printf("fsync: error: call to fsync - %s\n", strerror(errno));
